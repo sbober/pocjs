414ee77f0f28f5306e4db43c9dd5b3a7a6f5fe58
Sebastian Bober <sbober@servercare.de>
2011-09-08 10:10:24 +0200

    use closure compiler to compress the JS

--- scripts/combine.sh
+++ scripts/combine.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-#echo 'dojo.provide("pocjs.combined");' > pocjs/combined.js
 files=$( cat pocjs/all.js |grep require|sed -e 's/.*("\(.*\)").*/\1/'|sed 's/\./\//g' )
 for i in dojo/dojo.js.uncompressed zlib png $files
 do
     cat $i.js
-done > pocjs/combined.js
+done | java -jar scripts/compiler.jar > pocjs/combined.js
