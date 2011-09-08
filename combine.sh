65ca55b6d360e6b15d41b2041f19bb9c4675e912
Sebastian Bober <sbober@servercare.de>
2011-09-08 09:57:00 +0200

    update combining

--- combine.sh
+++ combine.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-echo 'dojo.provide("pocjs.combined");' > pocjs/combined.js
+#echo 'dojo.provide("pocjs.combined");' > pocjs/combined.js
 files=$( cat pocjs/all.js |grep require|sed -e 's/.*("\(.*\)").*/\1/'|sed 's/\./\//g' )
-for i in zlib png $files
+for i in dojo/dojo.js.uncompressed zlib png $files
 do
     cat $i.js
-done >> pocjs/combined.js
+done > pocjs/combined.js
