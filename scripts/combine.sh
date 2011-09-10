2e1e7bd6c99edb3b2cc8b3d072ce4de51fe8298f
Sebastian Bober <sbober@servercare.de>
2011-09-10 21:01:12 +0200

    just set a redirect to amazon s3 to fix issue #1

--- scripts/combine.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-files=$( cat pocjs/all.js |grep require|sed -e 's/.*("\(.*\)").*/\1/'|sed 's/\./\//g' )
-for i in dojo/dojo.js.uncompressed zlib png $files
-do
-    cat $i.js
-done | java -jar scripts/compiler.jar > pocjs/combined.js
